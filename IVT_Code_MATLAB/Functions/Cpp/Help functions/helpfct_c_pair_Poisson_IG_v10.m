fea4715cd6366af49d25b76c382f8c0e2fa01866
mbennedsen <80953990+mbennedsen@users.noreply.github.com>
2021-07-01 13:37:39 +0200

    Delete IVT_Code_MATLAB directory

--- IVT_Code_MATLAB/Functions/Cpp/Help functions/helpfct_c_pair_Poisson_IG_v10.m
+++
@@ -1,7 +0,0 @@
-function [loglik,grad] = helpfct_c_pair_Poisson_IG_v10(y,par,Hvec,H,n,dt)
-
-output = c_pair_grad_Poisson_IG_v10(y,par,Hvec,H,n,dt);
-
-loglik = -1*output(1);
-grad   = -1*exp(par).*output(2:4);
-%grad   = -1*output(2:4);
